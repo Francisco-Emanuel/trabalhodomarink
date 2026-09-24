f80eb827b197fb7d8dba5ce6a2908ce8d1d0eb26
Francisco <chico_emanuelsoares@hotmail.com>
2026-09-24 11:18:13 -0300

    feat: migrate application to TypeScript with Express, Sequelize, SQLite, and Jest tests

--- src/appjs/routes/produto.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const controller =
-require("../controllers/produto.controller");
-
-router.get("/", controller.listar);
-router.post("/", controller.criar);
-module.exports = router;
